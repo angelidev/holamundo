6ad394e7b4ec06268c4c89d18d22282f90ba6cc7
angelidev <angelimaria75@gmail.com>
2022-07-24 14:31:14 -0500

    Archivo JS que suma dos números

---
+++ sumar.js
@@ -0,0 +1,5 @@
+let num1 = 5; // num1 contiene el valor 3
+let num2 = 4; // num2 contiene el valor 4
+result = num1 + num2; // result ahora contiene el valor 7
+
+console.log("La suma de los dos número es: " + result);
